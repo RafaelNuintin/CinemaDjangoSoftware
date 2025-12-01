1a817273758a410ff76aba9e6a5d2c74194342a8
RafaelNuintin <rafael.nuintin@alunos.ifsuldeminas.edu.br>
2025-12-01 11:12:33 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from django.views import View
-from django.contrib import messages
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
-def post(self, request):
-    pass
+
+class FilmesView(View):
+    def get(self, request, *args, **kwargs):
+        filmes = Filme.objects.all()
+        return render(request, 'filmes.html', {'filmes': filmes})
+# def post(self, request, *args, **kwargs):
+class SeriesView(View):
+    def get(self, request, *args, **kwargs):
+        series = Serie.objects.all()
+        return render(request, 'series.html', {'series': serie})
+
+class EpisodiosView(View):
+    def get(self, request, *args, **kwargs):
+        episodios = Episodio.objects.all()
+        return render(request, 'episodios.html', {'episodios': episodios})
+class GenerosView(View):
+    def get(self, request, *args, **kwargs):
+        generos = Genero.objects.all()
+        return render(request, 'generos.html', {'generos': generos})
+class TemporadasView(View):
+    def get(self, request, *args, **kwargs):
+        temporadas = Temporada.objects.all()
+        return render(request, 'temporadas.html', {'temporadas': temporadas})
+class ElencoView(View):
+    def get(self, request, *args, **kwargs):
+        elencos = Elenco.objects.all()
+        return render(request, 'elenco.html', {'elencos': elencos})
+class AtuacaoView(View):
+    def get(self, request, *args, **kwargs):
+        atuacoes = Atuacao.objects.all()
+        return render(request, 'atuacoes.html', {'atuacoes': atuacoes})
